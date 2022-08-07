50fe4dc924dabd6d42b06251af555eba09d51b88
SageScroll18144 <lipe.fssantos063@gmail.com>
2022-08-07 20:30:59 -0300

    readme

---
+++ provas_mps/mps/README.md
@@ -0,0 +1,5 @@
+# GABARITO MPs IP-EC(IF669)
+
+- 2021.1 MP1:
+-> Questão: https://docs.google.com/document/d/1SCnU4NXJbqYwWZCXnCJpJBJN-Qe8ICrH/edit    
+-> Solução: [mp1_2021.c](https://github.com/SageScroll18144/IP-Func/blob/main/provas_mps/mps/mp1_2021.c)
--- provas_mps/mps/mp12020_2.c
+++ provas_mps/mps/mp12020_2.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 typedef struct Aluno{
     char nome[20];
@@ -17,16 +18,19 @@ typedef struct Turma{
 int main(){
 
     int flag=1;
+    turma *acm;
     while(flag){
         printf("Escolha uma opção abaixo:\n1)Inserção.\n2)Remoção de Aluno.\n3)Aprovados.\n4)Reprovados.\n5)Turmas.\n6)Sair do programa.\n");
         printf("Pedido.: ");
         int option;
         scanf("%d", &option);
+        
         switch(option){
             case 1:
                 printf("Informa a quantidade de alunos: ");
                 int n;
                 scanf("%d", &n);
+                acm.qtd=n;
                 printf("Para cada aluno informe: (Nome) (matricula) (notas) (qtd de faltas)\n");
                 aluno tmp;
                 while(n--){
@@ -37,8 +41,36 @@ int main(){
             case 2:
                 break;
             case 3:
+                printf("*Aprovados*\n");
+                char id_tmp[5];
+                scanf(" %s", id_tmp);
+                int idx=0;
+                while(strcmp(acm[idx++].id, id_tmp)!=0);
+                idx--;
+                for(int i=0;i<acm[idx].qtd;i++){
+                    float media=0;
+                    for(int j=0;j<5;j++){
+                        media+=acm[idx].alunos[i].vetor[j];
+                    }
+                    media = media / 5;
+                    if(media>=7) printf("%s\n", acm[idx].aluno[i].nome);
+                }
                 break;
             case 4:
+                printf("*Reprovados*\n");
+                char id_tmp[5];
+                scanf(" %s", id_tmp);
+                int idx=0;
+                while(strcmp(acm[idx++].id, id_tmp)!=0);
+                idx--;
+                for(int i=0;i<acm[idx].qtd;i++){
+                    float media=0;
+                    for(int j=0;j<5;j++){
+                        media+=acm[idx].alunos[i].vetor[j];
+                    }
+                    media = media / 5;
+                    if(media<3) printf("%s\n", acm[idx].aluno[i].nome);
+                }
                 break;
             case 5:
                 break;
